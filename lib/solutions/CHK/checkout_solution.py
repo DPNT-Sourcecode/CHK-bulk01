cdf4bc8097c651dc8638ccb58ee486298c9ff5ca
sbx_user1051 <sbx_user1051@169.254.45.87>
2025-12-10 17:42:41 +0000

    Wed Dec 10 17:42:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,12 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
+        price_table = {"A": {"price": }}
+
         total_cost = 0
 
         print(skus)
 
         return total_cost
+
 
