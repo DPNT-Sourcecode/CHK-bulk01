638df5d10ec11d9a63c3be9e9840bd67ba4acd88
sbx_user1051 <sbx_user1051@169.254.45.87>
2025-12-10 17:45:41 +0000

    Wed Dec 10 17:45:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,36 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        price_table = {"A": {"price": }}
-
+        price_table = {
+            "A": {
+                "price": 50, 
+                "deal": ''
+                },
+            "B": {
+                "price": 30, 
+                "deal": ''
+                },
+            "C": {
+                "price": 20
+            },
+            "D": {
+                "price": 15
+                }
+            }
         total_cost = 0
 
-        print(skus)
+        skus = [skus.count(sku) for sku in skus.split() if sku in price_table.keys()]
 
         return total_cost
+
+
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
 
 
